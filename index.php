b5d10f910113a951b9b4b35949ebe14b07f9e0cf
Philippe Traverse <philippe.traverse@gmail.com>
2015-06-15 23:01:20 -0700

    updating reamde2

---
+++ index.php
@@ -0,0 +1,14 @@
+ <!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="UTF-8">
+  <title>title</title>
+  <link rel="stylesheet" href="public/css/stylesheet.css" type="text/css">
+ </head>
+ <body>
+  
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.21/jquery-ui.min.js" type="text/javascript"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.5.3/modernizr.min.js" type="text/javascript"></script>
+ </body>
+</html>
