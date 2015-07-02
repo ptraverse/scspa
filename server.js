2f72ff8e894c246a3bacd2ca591c97bb30d54c00
Philippe Traverse <philippe.traverse@gmail.com>
2015-07-02 14:55:29 -0700

    transferred to other laptop

--- server.js
+++ server.js
@@ -2,30 +2,27 @@ var express = require('express');
 var app = express();
 var jade = require('jade');
 
+//Express Settings
 app.set('views', __dirname + '/templates');
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
+//Routes
+//Route: Root
 app.get('/', function (req, res) {
   console.log("Hello World!");
   var html = jade.renderFile('templates/index.jade');
 	res.send(html);
 });
-
-app.get('/test', function (req, res) {
-	res.send('Something Else!');
-});
-
+//Route: tt "totaltweets"
 app.get('/tt', function (req, res) {
 	var html = jade.renderFile('templates/total_tweets.jade');
 	res.send(html);
 });
 
+//Initialize Server
 var server = app.listen(3000, function () {
-
   var host = server.address().address;
   var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
+  console.log('App online at http://%s:%s', host, port);
 });
